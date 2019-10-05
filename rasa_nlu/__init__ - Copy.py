db9f1a1f52c279391f2b129db61f101b684c7128
aisensevision <aisense.vision@gmail.com>
2019-10-05 16:40:16 +0700

    project / model management
    NLU model train, project/model management

--- rasa_nlu/__init__ - Copy.py
+++ rasa_nlu/__init__ - Copy.py
@@ -1,10 +1,10 @@
-import rasa_nlu
+import rasa.nlu
 import sys
 import warnings
 
 # this makes sure old code can still import from `rasa_nlu`
 # although the package has been moved to `rasa.nlu`
-#sys.modules["rasa_nlu"] = rasa.nlu
+sys.modules["rasa_nlu"] = rasa.nlu
 
 warnings.warn(
     "The 'rasa_nlu' package has been renamed. You should change "
